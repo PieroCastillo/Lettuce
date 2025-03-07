4eacd12d679c227074d868092eeca25866380773
Piero Castillo <pierocastillo2005@gmail.com>
2025-03-06 23:03:03 -0500

    first version of Shader (VkShaderEXT)

--- Lettuce/Core/Shader.cpp
+++ Lettuce/Core/Shader.cpp
@@ -4,36 +4,72 @@
 #include "Lettuce/Core/common.hpp"
 #include <iostream>
 #include <vector>
+#include <string>
 #include <memory>
 #include "Lettuce/Core/Descriptors.hpp"
+#include "Lettuce/Core/PipelineLayout.hpp"
 #include "Lettuce/Core/Device.hpp"
 #include "Lettuce/Core/Utils.hpp"
 #include "Lettuce/Core/Shader.hpp"
 
 using namespace Lettuce::Core;
 
-void Shader::Assemble(const VkShaderStageFlagBits& stage, const std::shared_ptr<Descriptors>& descriptors)
+void Shader::AddConstant(uint32_t constantId, uint32_t size, void *data)
 {
-    //    VkStructureType sType;
-    // const void *pNext;
-    // VkShaderCreateFlagsEXT flags;
-    // ;
-    // VkShaderStageFlags nextStage;
-    // VkShaderCodeTypeEXT codeType;
-    // size_t codeSize;
-    // const void *pCode;
-    // const char *pName;
-    // uint32_t setLayoutCount;
-    // const VkDescriptorSetLayout *pSetLayouts;
-    // uint32_t pushConstantRangeCount;
-    // const VkPushConstantRange *pPushConstantRanges;
-    // const VkSpecializationInfo *pSpecializationInfo;
+    VkSpecializationMapEntry entry = {
+        .constantID = constantId,
+        .offset = currentOffset,
+        .size = size,
+    };
+    entries.push_back(entry);
+    datas.push_back(data);
+
+    currentOffset += size;
+}
+
+void Shader::Assemble(const VkShaderStageFlagBits &stage, const std::vector<uint32_t> &code)
+{
     VkShaderCreateInfoEXT shaderCI = {
         .sType = VK_STRUCTURE_TYPE_SHADER_CREATE_INFO_EXT,
         .stage = stage,
+        .codeType = VK_SHADER_CODE_TYPE_SPIRV_EXT, // this is the most common format
+        .codeSize = sizeof(uint32_t) * code.size(),
+        .pCode = (void *)code.data(),
+        .setLayoutCount = (uint32_t)_descriptors->_layouts.size(),
+        .pSetLayouts = _descriptors->_layouts.data(),
+        .pushConstantRangeCount = (uint32_t)_layout->pushConstants.size(),
+        .pPushConstantRanges = _layout->pushConstants.data()};
 
-        
-        .setLayoutCount = (uint32_t)descriptors->_layouts.size(),
-        .pSetLayouts = descriptors->_layouts.data(),
-    };
+    // here we add the specialization constants
+    void *data;
+    if (entries.size() > 0)
+    {
+        VkSpecializationInfo info;
+        info.mapEntryCount = (uint32_t)entries.size();
+        info.pMapEntries = entries.data();
+
+        uint32_t fullsize = 0;
+        for (auto &entry : entries)
+        {
+            fullsize += entry.size;
+        }
+
+        int i = 0;
+        data = malloc(fullsize);
+        char *iter = (char *)data;
+        for (auto &partialData : datas)
+        {
+            memcpy((void *)iter, partialData, entries[i].size);
+            iter += entries[i].size;
+            i++;
+        }
+        info.dataSize = fullsize;
+        info.pData = data;
+        shaderCI.pSpecializationInfo = &info;
+    }
+    checkResult(vkCreateShadersEXT(_device->_device, 1, &shaderCI, nullptr, &_shader));
+    if (entries.size() > 0)
+    {
+        free(data);
+    }
 }
--- include/Lettuce/Core/Shader.hpp
+++ include/Lettuce/Core/Shader.hpp
@@ -6,17 +6,31 @@
 #include <vector>
 #include <memory>
 #include "Device.hpp"
+#include "Descriptors.hpp"
+#include "PipelineLayout.hpp"
 #include "Utils.hpp"
 
 namespace Lettuce::Core
 {
     class Shader
     {
-    public:
-        VkShaderEXT _executionSet;
-        std::shared_ptr<Device> _device;
+    private:
+        std::vector<VkSpecializationMapEntry> entries;
+        std::vector<void*> datas; 
+        uint32_t currentOffset = 0;
 
-        Shader(const std::shared_ptr<Device>& device) : _device(device){}
-        void Assemble(const VkShaderStageFlagBits& stage, const std::shared_ptr<Descriptors>& descriptors);
+    public:
+        VkShaderEXT _shader;
+        std::shared_ptr<Device> _device;
+        std::shared_ptr<PipelineLayout> _layout;
+        std::shared_ptr<Descriptors> _descriptors;
+
+        Shader(const std::shared_ptr<Device> &device, const std::shared_ptr<PipelineLayout> &layout, const std::shared_ptr<Descriptors> &descriptors) : _device(device), _layout(layout), _descriptors(descriptors)
+        {
+        }
+
+        void Assemble(const VkShaderStageFlagBits &stage, const std::vector<uint32_t> &code);
+
+        void AddConstant(uint32_t constantId, uint32_t size, void *data);
     };
 }
