dc2d7dcbc7dcecf6cc78dbec375204a1c566c3b0
Piero Castillo <pierocastillo2005@gmail.com>
2025-04-08 12:47:46 -0500

    use xmake instead of CMake

--- include/Lettuce/Core/common.hpp
+++ include/Lettuce/Core/common.hpp
@@ -2,7 +2,7 @@
 
 #define NOMINMAX
 #include "volk.h"
-#include "vma/vk_mem_alloc.h"
+#include < vma/vk_mem_alloc.h>
 #ifdef LETTUCE_EXPERIMENTAL
 #include <vulkan/vulkan_beta.h>
 #endif
--- xmake.lua
+++ xmake.lua
@@ -1,10 +1,17 @@
-set_languages("c++20")
+set_languages("c++23")
+set_runtimes("MD") 
 
 add_requires("vulkansdk")
-add_requires("shaderc")
+add_requires("shaderc", {
+    configs = {
+        binaryonly = false,
+        shared = false     
+    }
+})
 add_requires("volk")
 add_requires("vulkan-memory-allocator")
 add_requires("glfw")
+add_requires("glm")
 add_requires("fastgltf")
 add_requires("directxshadercompiler")
 
@@ -12,13 +19,14 @@ local v = ("0.0.1.0"):split("%.")
 add_defines("VARIANT_VERSION=" .. (v[1] or 0),
             "MAJOR_VERSION="   .. (v[2] or 0),
             "MINOR_VERSION="   .. (v[3] or 0),
-            "PATCH_VERSION="   .. (v[4] or 0))
+            "PATCH_VERSION="   .. (v[4] or 0),
+            "VOLK_STATIC_DEFINES")
 
 if is_os("windows") then
     add_links("user32", "gdi32") 
-    add_defines("VOLK_STATIC_DEFINES", "VK_USE_PLATFORM_WIN32_KHR", "_WIN32")
+    add_defines("VK_USE_PLATFORM_WIN32_KHR", "_WIN32")
 else
-    add_defines("VOLK_STATIC_DEFINES", "VK_USE_PLATFORM_WAYLAND_KHR")
+    add_defines("VK_USE_PLATFORM_WAYLAND_KHR")
 end
 
 target("Lettuce")
@@ -36,4 +44,22 @@ target("Lettuce")
     add_files("Lettuce/Core/Compilers/*.cpp")
     add_files("Lettuce/X3D/Geometries/*.cpp")
     -- add_files("Lettuce/X3D/Materials/*.cpp")
-    add_packages("vulkansdk", "volk", "vulkan-memory-allocator", "glfw", "fastgltf", "shaderc", "directxshadercompiler")
+    add_packages("vulkansdk", "volk", "vulkan-memory-allocator", "glfw", "glm", "fastgltf", "shaderc", "directxshadercompiler", {public = true})
+    
+local samples = {
+    "ClearScreenSample",
+    "DonutSample",
+    "GeometryIndirectSample",
+    "LoadModel",
+    "SampleX2D",
+    "ShadowMaps"
+}
+
+for _, name in ipairs(samples) do 
+    target(name)
+        set_kind("binary")
+        add_includedirs("samples/SampleFramework", "external/stb","include")
+        add_files("samples/" .. name .. "/app.cpp")
+        add_deps("Lettuce")
+        add_packages("volk", "glfw", "glm")
+end
