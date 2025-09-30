6b942b47583d2409e52ca6e500dcd35549654641
PieroCastillo <39932324+PieroCastillo@users.noreply.github.com>
2025-09-30 10:02:26 -0500

    add ktx to xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -9,6 +9,7 @@ add_requires("glfw")
 add_requires("glm")
 add_requires("fastgltf")
 add_requires("imgui")
+add_requires("ktx")
 
 local v = ("0.0.1.0"):split("%.")
 add_defines("VARIANT_VERSION=" .. (v[1] or 0),
@@ -29,7 +30,7 @@ target("Lettuce")
     add_includedirs("include/")
     add_headerfiles("include/Lettuce/**.hpp")
     add_files("src/Core/**.cpp")
-    add_packages("vulkansdk", "volk", "glfw", "glm", "fastgltf",{public = true})
+    add_packages("vulkansdk", "volk", "glfw", "ktx", "glm", "fastgltf",{public = true})
     
 local samples = {
         "helloTriangle",
