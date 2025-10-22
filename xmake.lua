ed254fb1102a79bf60d774003aca56b85babeb52
PieroCastillo <39932324+PieroCastillo@users.noreply.github.com>
2025-10-22 11:00:52 -0500

    solve symbol conflicts

--- xmake.lua
+++ xmake.lua
@@ -1,10 +1,10 @@
 set_languages("c++23")
-set_toolchains("clang-cl")
 
+add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode"})
 
 add_requires("vulkansdk")
-add_requires("volk")
+add_requires("volk", { header_only=true })
 add_requires("glfw")
 add_requires("glm")
 add_requires("fastgltf")
@@ -15,39 +15,32 @@ local v = ("0.0.1.0"):split("%.")
 add_defines("VARIANT_VERSION=" .. (v[1] or 0),
             "MAJOR_VERSION="   .. (v[2] or 0),
             "MINOR_VERSION="   .. (v[3] or 0),
-            "PATCH_VERSION="   .. (v[4] or 0),
-            "VOLK_STATIC_DEFINES")
+            "PATCH_VERSION="   .. (v[4] or 0))
 
 if is_os("windows") then
-    add_links("user32", "gdi32") 
+    add_links("user32", "gdi32", "kernel32") 
     add_defines("VK_USE_PLATFORM_WIN32_KHR", "_WIN32")
 else
     add_defines("VK_USE_PLATFORM_WAYLAND_KHR")
 end
 
 target("Lettuce")
-    set_kind("static")
+    set_kind("shared")
     add_includedirs("include/")
     add_headerfiles("include/Lettuce/**.hpp")
     add_files("src/Core/**.cpp")
-    add_packages("vulkansdk", "volk", "glfw", "ktx", "glm", "fastgltf",{public = true})
+    add_packages("volk", "glfw", "ktx", "glm", "fastgltf")
+    add_rules("utils.symbols.export_all", {export_classes = true})
     
 local samples = {
-        "helloTriangle",
---     "ClearScreenSample",
---     "DonutSample",
---     "GeometryIndirectSample",
---     "LoadModel",
---     "SampleX2D",
---     "ShadowMaps",
---     "SkyBox",
+    "helloTriangle",
 }
 
 for _, name in ipairs(samples) do 
     target(name)
         set_kind("binary")
+        add_deps("Lettuce")
         add_includedirs("include")
         add_files("samples/" .. name .. "/app.cpp")
-        add_deps("Lettuce")
         add_packages("volk", "glfw", "glm", "imgui")
 end
