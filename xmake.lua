12316e837b37ee1f0268a76dcbad656ede6f35bf
Piero Castillo <pierocastillo2005@gmail.com>
2025-11-02 14:58:17 -0500

    improve slang shaders compilation at xmake build

--- xmake.lua
+++ xmake.lua
@@ -11,7 +11,7 @@ add_requires("fastgltf")
 add_requires("imgui")
 add_requires("ktx", { configs={ vulkan=true, ktx2=true, decoder=true} })
 add_requires("slang", { configs={ slangc=true }})
-add_requires("spirv-reflect", { configs = { shared=true }})
+add_requires("spirv-reflect")
 
 local v = ("0.0.1.0"):split("%.")
 add_defines("VARIANT_VERSION=" .. (v[1] or 0),
@@ -33,7 +33,7 @@ target("Lettuce")
     add_files("src/Core/**.cpp")
     add_packages("volk", "glfw", "ktx", "glm", "fastgltf", "slang", "spirv-reflect")
     add_rules("utils.symbols.export_all", {export_classes = true})
-    
+
 local samples = {
     "helloTriangle",
 }
@@ -48,37 +48,28 @@ for _, name in ipairs(samples) do
         
         after_build(function (target)
             import("core.base.process")
-            -- local slangc = find_tool("slangc") or find_tool("slangc.exe")
-            -- if not slangc then
-            --     print("warning: 'slangc' not found in PATH; skipping .slang compilation for target '" .. target:name() .. "'")
-            --     return
-            -- end
 
-            -- search for .slang files under the project tree
             local slang_files = os.files("**/*.slang")
             if #slang_files == 0 then
                 return
             end
 
-            -- Use target's binary directory as output
             local outdir = path.absolute(target:targetdir())
             os.mkdir(outdir)
 
-            -- Compile each .slang file to SPIR-V
             for _, f in ipairs(slang_files) do
                 local outfile = path.join(outdir, path.basename(f) .. ".spv")
-                -- Try compiling to SPIR-V with Vulkan target
-                local ok = process.set_runargs({ 
-                    "slangc",
-                    f,
-                    "-profile", "glsl_450",
-                    "-target", "spirv",      -- Output format
-                    "-o", outfile,           -- Output file
-                })
-                if ok ~= 0 then
+
+                local args = { '-fspv-reflect', '-preserve-params', f, "-profile", "glsl_450", "-target", "spirv", "-o", outfile }
+
+                local proc = process.openv("slangc", args)
+                local ok, status = proc:wait()
+                proc:close()
+
+                if ok < 0 then
                     print(string.format("warning: failed to compile %s", f))
                 else
-                    print(string.format("compiled %s -> %s", f, outfile))
+                    print(string.format("compiled %s -> %s", f, path.relative(outfile, outdir)))
                 end
             end
         end)
